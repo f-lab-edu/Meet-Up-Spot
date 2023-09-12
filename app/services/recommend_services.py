fb6e35d5f2b06621edd645dad58e1f82315eb4c1
minwoo <minwoo9621@gmail.com>
2023-09-12 22:29:01 +0900

    Add recommend_services and midpoint_services

---
+++ app/services/recommend_services.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from sqlalchemy.orm import Session
+
+from app.schemas.google_maps_api import GeocodeResponse
+
+from .google_maps_services import GoogleMapsService
+from .midpoint_services import calculate_midpoint_from_addresses
+
+
+# TODO: 추천 알고리즘 구현 (기본적으로 place rating 기반, 그리고 카페를 우선순위로)
+class Recommender:
+    def __init__(self):
+        self.maps_service = GoogleMapsService()
+
+    def recommend_places(
+        self, db: Session, addresses: List[str], radius: int = 1500
+    ) -> List[dict]:
+        try:
+            if len(addresses) == 1:
+                geocoded_address = self.maps_service.geocode_address(addresses[0])
+                places = self.maps_service.search_nearby_places(
+                    db, geocoded_address.latitude, geocoded_address.longitude, radius
+                )
+            else:
+                midpoint: GeocodeResponse = calculate_midpoint_from_addresses(addresses)
+
+                if midpoint:
+                    places = self.maps_service.search_nearby_places(
+                        db, midpoint.latitude, midpoint.longitude, radius
+                    )
+                else:
+                    raise Exception("Failed to calculate midpoint.")
+            return places
+        except Exception as e:
+            print(f"Error while recommending places: {str(e)}")
+            return []

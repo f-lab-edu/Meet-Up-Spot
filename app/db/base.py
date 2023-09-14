ffe7e7a92511b4cdbb496c3f3e784a8b850022df
minwoo <minwoo9621@gmail.com>
2023-09-14 15:32:17 +0900

    Add database-related code and migrations for test

---
+++ app/db/base.py
@@ -0,0 +1,5 @@
+from app.db.base_class import Base
+from app.models.google_maps_api_log import GoogleMapsApiLog
+from app.models.location import Location
+from app.models.place import Place
+from app.models.user import User

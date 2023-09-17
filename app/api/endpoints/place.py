cbfec5adcc56cec55615a40d301e4fd3cd64e24d
minwoo <minwoo9621@gmail.com>
2023-09-17 22:58:45 +0900

    Add other endpoints in places

--- app/api/endpoints/place.py
+++ app/api/endpoints/place.py
@@ -1,10 +1,12 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from app import crud, models
 from app.api.deps import get_db
-from app.schemas.place import Place
+from app.schemas.place import AutoCompletedPlace, Place
+from app.services import user_service
 from app.services.google_maps_services import GoogleMapsService
 from app.services.recommend_services import Recommender
 
@@ -14,26 +16,90 @@ goggle_maps_service = GoogleMapsService()
 recommend_service = Recommender()
 
 
-@router.post("/request_places/", response_model=List[Place])
+# TODO: 중간장소 계산만 하는 엔드포인트랑 나눠야함 밑에 여기는 추천만 하는거
+@router.post("/request-places/", response_model=List[Place])
 async def request_places(addresses: List[str], db: Session = Depends(get_db)):
     """
-    유저에게 주소를 입력받아 만남 장소를 추천하는 API 엔드포인트
+    request meeting places
     """
     complete_addresses = []
     for address in addresses:
         complete_addresses.append(
-            goggle_maps_service.auto_complete_place(address)[0]["description"]
+            goggle_maps_service.auto_complete_place(address)[0].address
         )
 
     results: List[Place] = recommend_service.recommend_places(db, complete_addresses)
     return results
 
 
-@router.get("/select_place/")
-async def select_place(place_id: str, db: Session = Depends(get_db)):
+# TODO: 여기는 그냥 기본적으로 장소를 받아서 중간 지점 계산
+# 장소 받아서 중간 지점 계산
+@router.post("/request-midpoint/", response_model=List[Place])
+async def request_midpoint(places: List[Place], db: Session = Depends(get_db)):
+    pass
+
+
+@router.get("/{place_id}", response_model=Place)
+async def read_place_by_id(
+    place_id: str,
+    current_user: models.User = Depends(user_service.get_current_active_user),
+    db: Session = Depends(get_db),
+):
     """
-    유저가 선택한 장소를 저장하는 API 엔드포인트
+    Retrieve place.
+    """
+    place = crud.place.get_by_place_id(db, id=place_id)
+
+    if not place:
+        raise HTTPException(status_code=404, detail="Place not found")
+
+    return place
+
+
+# TODO: 유저 place 타입 기반으로 필터링 하거나 엔드포인트 추가
+@router.get("/", response_model=List[Place])
+async def read_places(
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(user_service.get_current_active_user),
+):
+    """
+    Retrieve places.
+    """
+
+    places = crud.place.get_multi(db)
+    return places
+
+
+@router.post("/{place_id}/mark", response_model=Place)
+async def mark_interest(
+    place_id: str,
+    current_user: models.User = Depends(user_service.get_current_active_user),
+    Session=Depends(get_db),
+):
+    """
+    mark up place.
+
     """
     pass
-    # 어차피 db에 있는것만 선택할 수 있으니까 그냥 place_id만 받아서 저장하면 될듯
-    # place_id로 장소를 검색해서 있으면 저장하고 없으면 에러를 반환하면 될듯
+
+
+@router.delete("/{place_id}/unmark", response_model=Place)
+async def unmark_interest(
+    place_id: str,
+    current_user: models.User = Depends(user_service.get_current_active_user),
+    Session=Depends(get_db),
+):
+    """
+    unmark place.
+
+    """
+    pass
+
+
+@router.get("/completed-places/{address}", response_model=List[AutoCompletedPlace])
+async def read_auto_completed_places(address: str, db: Session = Depends(get_db)):
+    """
+    retrieve some auto completed places
+    """
+    places = goggle_maps_service.auto_complete_place(address)
+    return places
--- app/api/routers.py
+++ app/api/routers.py
@@ -4,16 +4,16 @@ from app.core.config import get_app_settings
 from app.core.settings.base import AppEnvTypes
 
 from .endpoints import goolge_maps_api_test as api_test
-from .endpoints import login, place, user
+from .endpoints import login, places, users
 
 settings = get_app_settings()
 
 api_router = APIRouter()
 api_router.include_router(
-    place.router, prefix="/places", tags=["google_maps"]
+    places.router, prefix="/places", tags=["places"]
 )  # geocode 라우터 추가
 api_router.include_router(login.router, tags=["login"])
-api_router.include_router(user.router, prefix="/users", tags=["user"])
-api_router.include_router(user.admin_router, prefix="/admin", tags=["admin"])
+api_router.include_router(users.router, prefix="/users", tags=["user"])
+api_router.include_router(users.admin_router, prefix="/admin", tags=["admin"])
 if settings.APP_ENV == AppEnvTypes.dev:
     api_router.include_router(api_test.test_router, prefix="/test", tags=["test"])

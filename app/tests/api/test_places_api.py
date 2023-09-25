15b42d71aacdeae0cc9c44301915dd1d0fde38e4
minwoo <minwoo9621@gmail.com>
2023-09-25 23:38:45 +0900

    Modify test case

--- app/tests/api/test_places_api.py
+++ app/tests/api/test_places_api.py
@@ -1,10 +1,14 @@
 from typing import Dict
-from unittest.mock import ANY, patch
+from unittest.mock import ANY, create_autospec, patch
 
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
+from app import crud
 from app.core.settings.app import AppSettings
+from app.db.session import SessionLocal
+from app.services.recommend_services import Recommender
+from app.tests.conftest import get_global_db_override
 from app.tests.utils.places import (
     auto_completed_place_schema,
     mock_geocode_response,
@@ -16,36 +20,27 @@ from app.tests.utils.utils import random_email, random_lower_string
 
 
 def test_request_places(client: TestClient, db: Session, settings: AppSettings):
+    mock_recommender = create_autospec(Recommender)
+    mock_recommender.recommend_places.return_value = [mock_place_obj]
+
     with patch(
         "app.api.endpoints.places.map_services.get_complete_addresses",
         return_value=[test_address],
     ) as mock_get_complete, patch(
-        "app.api.endpoints.places.recommend_services.recommend_places",
-        return_value=[mock_place_obj],
-    ) as mock_recommend:
+        "app.api.endpoints.places.Recommender", return_value=mock_recommender
+    ):
         response = client.post(
             f"{settings.API_V1_STR}/places/request-places/", json=[test_address]
         )
-        assert response.status_code == 200
-        assert len(response.json()) == 1
-        mock_get_complete.assert_called_once()
-        mock_recommend.assert_called_once()
 
-
-def test_request_midpoint(client: TestClient, db: Session, settings: AppSettings):
-    with patch(
-        "app.api.endpoints.places.map_services.get_complete_addresses",
-        return_value=[test_address],
-    ) as mock_get_complete, patch(
-        "app.api.endpoints.places.recommend_services.recommend_places",
-        return_value=mock_geocode_response,
-    ) as mock_recommend:
-        response = client.post(
-            f"{settings.API_V1_STR}/places/request-midpoint/", json=[test_address]
-        )
         assert response.status_code == 200
+        assert response.json() == [mock_place_obj.model_dump()]
+
+        # get_complete_addresses가 한 번 호출되었는지 확인합니다.
         mock_get_complete.assert_called_once()
-        mock_recommend.assert_called_once()
+
+        # recommend_places 메소드가 한 번 호출되었는지 확인합니다.
+        mock_recommender.recommend_places.assert_called_once()
 
 
 def test_read_place_by_id(
@@ -86,19 +81,20 @@ def test_read_places(
 
 
 def test_read_auto_completed_places(
-    client: TestClient, db: Session, settings: AppSettings
+    client: TestClient, settings: AppSettings, normal_user_token_headers
 ):
     with patch(
         "app.services.map_services.MapServices.auto_complete_place",
         return_value=[auto_completed_place_schema],
     ) as mock_auto_complete:
         response = client.get(
-            f"{settings.API_V1_STR}/places/completed-places/{test_address}"
+            f"{settings.API_V1_STR}/places/completed-places/{test_address}",
+            headers=normal_user_token_headers,
         )
 
         assert response.status_code == 200
         assert len(response.json()) == 1
-        mock_auto_complete.assert_called_once_with(test_address)
+        mock_auto_complete.assert_called_once_with(ANY, ANY, test_address)
 
 
 def test_get_distance_matrix(client: TestClient, db: Session, settings: AppSettings):
--- app/tests/services/test_map_services.py
+++ app/tests/services/test_map_services.py
@@ -3,51 +3,14 @@ from unittest.mock import MagicMock
 import pytest
 
 from app import crud
-from app.services.map_services import MapServices, ZeroResultException
+from app.schemas.google_maps_api import DistanceInfo
+from app.services.map_services import CustomException, MapServices, ZeroResultException
 from app.tests.utils.places import (
     mock_geocode_response,
     mock_location,
     mock_place_api_response,
     mock_place_obj,
 )
-
-
-def test_get_lat_lng_from_address(map_service: MapServices):
-    map_service.map_adapter.geocode_address = MagicMock(
-        return_value=mock_geocode_response
-    )
-
-    response = map_service.get_lat_lng_from_address("Test Address")
-
-    assert response.latitude == 123.456
-    assert response.longitude == 789.101
-
-
-def test_get_lat_lng_from_address_no_result(map_service: MapServices):
-    map_service.map_adapter.geocode_address = MagicMock(return_value=[])
-
-    with pytest.raises(ZeroResultException):
-        map_service.get_lat_lng_from_address("Invalid Address")
-
-
-@pytest.mark.parametrize("db", [MagicMock()])
-def test_create_or_get_location_existing(map_service: MapServices, db):
-    db.query().filter().first.return_value = mock_location
-
-    result = map_service.create_or_get_location(db, mock_geocode_response[0])
-
-    assert result == mock_location
-
-
-@pytest.mark.parametrize("db", [MagicMock()])
-def test_create_or_get_location_new(map_service: MapServices, db):
-    db.query().filter().first.return_value = None
-    crud.location.create = MagicMock(return_value=mock_location)
-
-    result = map_service.create_or_get_location(db, mock_geocode_response[0])
-
-    assert result == mock_location
-    crud.location.create.assert_called()  # 생성 메서드가 호출되었는지 확인
 
 
 @pytest.mark.parametrize("db", [MagicMock()])
@@ -73,3 +36,164 @@ def test_create_or_get_place_new(map_service: MapServices, db):
     assert result == mock_place_obj
     db.query().filter().first.assert_called_once()
     crud.place.create.assert_called()
+
+
+def test_get_lat_lng_from_address(map_service: MapServices, db, normal_user):
+    map_service.map_adapter.geocode_address = MagicMock(
+        return_value=mock_geocode_response
+    )
+
+    response = map_service.get_lat_lng_from_address(db, normal_user, "Test Address")
+
+    assert response.latitude == 123.456
+    assert response.longitude == 789.101
+
+
+def test_get_lat_lng_from_address_no_result(map_service: MapServices, db, normal_user):
+    map_service.map_adapter.client.geocode = MagicMock(return_value=[])
+
+    with pytest.raises(CustomException):
+        map_service.get_lat_lng_from_address(db, normal_user, "Invalid Address")
+
+
+@pytest.mark.parametrize("db", [MagicMock()])
+def test_create_or_get_location_existing(map_service: MapServices, db, normal_user):
+    db.query().filter().first.return_value = mock_location
+
+    result = map_service.create_or_get_location(db, mock_geocode_response[0])
+
+    assert result == mock_location
+
+
+@pytest.mark.parametrize("db", [MagicMock()])
+def test_create_or_get_location_new(map_service: MapServices, db):
+    db.query().filter().first.return_value = None
+    crud.location.create = MagicMock(return_value=mock_location)
+
+    result = map_service.create_or_get_location(db, mock_geocode_response[0])
+
+    assert result == mock_location
+    crud.location.create.assert_called()  # 생성 메서드가 호출되었는지 확인
+
+
+def test_get_distance_matrix_for_places_one_destination(map_service, db, normal_user):
+    distance_info_1 = (
+        DistanceInfo(
+            origin="origin_1",
+            destination="destination_1",
+            distance_text="10km",
+            distance_value=10000,
+            duration_text="20분",
+            duration_value=1200,
+        ),
+    )
+    distance_info_2 = (
+        DistanceInfo(
+            origin="origin_2",
+            destination="destination_1",
+            distance_text="15km",
+            distance_value=15000,
+            duration_text="30분",
+            duration_value=1800,
+        ),
+    )
+
+    map_service.map_adapter.calculate_distance_matrix = MagicMock(
+        return_value=[distance_info_1, distance_info_2]
+    )
+
+    response = map_service.get_distance_matrix_for_places(
+        db, normal_user, ["origin_1", "origin_2"], ["destination_1"], "transit"
+    )
+
+    assert response == [distance_info_1, distance_info_2]
+    map_service.map_adapter.calculate_distance_matrix.assert_called_once()
+
+
+def test_calculate_distance_matrix(db, normal_user, map_service):
+    map_service.map_adapter.client = MagicMock()
+    map_service.map_adapter.client.distance_matrix.return_value = {
+        "destination_addresses": [
+            "대한민국 경기도 성남시 분당구 삼평동 판교역로 160 판교역",
+            "대한민국 서울특별시 중구 소공동 세종대로18길 2 서울역",
+        ],
+        "origin_addresses": ["대한민국 경기도 성남시 분당구 성남대로 지하 601 서현", "대한민국 서울특별시 양재역"],
+        "rows": [
+            {
+                "elements": [
+                    {
+                        "distance": {"text": "2.2 km", "value": 2186},
+                        "duration": {"text": "22분", "value": 1299},
+                        "status": "OK",
+                    },
+                    {
+                        "distance": {"text": "29.4 km", "value": 29450},
+                        "duration": {"text": "53분", "value": 3168},
+                        "status": "OK",
+                    },
+                ]
+            },
+            {
+                "elements": [
+                    {
+                        "distance": {"text": "12.9 km", "value": 12881},
+                        "duration": {"text": "13분", "value": 805},
+                        "status": "OK",
+                    },
+                    {
+                        "distance": {"text": "15.5 km", "value": 15475},
+                        "duration": {"text": "34분", "value": 2065},
+                        "status": "OK",
+                    },
+                ]
+            },
+        ],
+        "status": "OK",
+    }
+
+    response = map_service.map_adapter.calculate_distance_matrix(
+        db,
+        normal_user,
+        origins=["대한민국 경기도 성남시 분당구 성남대로 지하 601 서현", "대한민국 서울특별시 양재역"],
+        destinations=[
+            "대한민국 경기도 성남시 분당구 삼평동 판교역로 160 판교역",
+            "대한민국 서울특별시 중구 소공동 세종대로18길 2 서울역",
+        ],
+        mode="transit",
+        language="ko",
+    )
+
+    assert response == [
+        DistanceInfo(
+            origin="대한민국 경기도 성남시 분당구 성남대로 지하 601 서현",
+            destination="대한민국 경기도 성남시 분당구 삼평동 판교역로 160 판교역",
+            distance_text="2.2 km",
+            distance_value=2186,
+            duration_text="22분",
+            duration_value=1299,
+        ),
+        DistanceInfo(
+            origin="대한민국 경기도 성남시 분당구 성남대로 지하 601 서현",
+            destination="대한민국 서울특별시 중구 소공동 세종대로18길 2 서울역",
+            distance_text="29.4 km",
+            distance_value=29450,
+            duration_text="53분",
+            duration_value=3168,
+        ),
+        DistanceInfo(
+            origin="대한민국 서울특별시 양재역",
+            destination="대한민국 경기도 성남시 분당구 삼평동 판교역로 160 판교역",
+            distance_text="12.9 km",
+            distance_value=12881,
+            duration_text="13분",
+            duration_value=805,
+        ),
+        DistanceInfo(
+            origin="대한민국 서울특별시 양재역",
+            destination="대한민국 서울특별시 중구 소공동 세종대로18길 2 서울역",
+            distance_text="15.5 km",
+            distance_value=15475,
+            duration_text="34분",
+            duration_value=2065,
+        ),
+    ]

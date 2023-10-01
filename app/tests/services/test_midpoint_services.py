f678bd9aed5e8a6aee7b0121e4a4335f9261c3fd
minwoo <minwoo9621@gmail.com>
2023-10-01 22:31:08 +0900

    Modify existing module test case and add filters and routes_matrix services test case

--- app/tests/services/test_midpoint_services.py
+++ app/tests/services/test_midpoint_services.py
@@ -1,17 +1,10 @@
-# mock 필요한 부분, 예를 들면 MapServices를 mock 하려면
-from unittest.mock import MagicMock
-
-from app.schemas.google_maps_api import DistanceInfo, GeocodeResponse
-from app.services.constants import AGGREGATED_ATTR
+from app.schemas.google_maps_api import GeocodeResponse
 from app.services.midpoint_services import (
-    DestinationSummary,
     calculate_midpoint_arithmetic,
     calculate_midpoint_from_addresses,
     calculate_midpoint_harvarsine,
     harversine_distance,
-    sort_destinations_by_aggregated_attr,
 )
-from app.tests.utils.places import distance_info_list
 
 
 def test_calculate_midpoint():
@@ -59,20 +52,3 @@ def test_harversine_distance():
     distance = harversine_distance(loc1, loc2)
 
     assert int(distance) == 141936
-
-
-def test_sort_destinations_by_aggregated_attr():
-    res = sort_destinations_by_aggregated_attr(
-        distance_info_list,
-        AGGREGATED_ATTR.DURATION,
-        2,
-    )
-
-    assert res == [
-        DestinationSummary(
-            destination_id="ChIJN1t_tDeuEmsRUsoyG83frY1", total_value=2104
-        ),
-        DestinationSummary(
-            destination_id="ChIJN1t_tDeuEmsRUsoyG83frY2", total_value=5233
-        ),
-    ]

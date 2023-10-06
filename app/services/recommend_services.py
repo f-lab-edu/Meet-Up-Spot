7be634889bb52922a98b52678889eee984cab381
minwoo <minwoo9621@gmail.com>
2023-10-06 23:39:01 +0900

    Modify rank candidate and add computating recommendation score logic

--- app/services/recommend_services.py
+++ app/services/recommend_services.py
@@ -1,8 +1,9 @@
-from typing import List
+from datetime import datetime
+from difflib import SequenceMatcher
+from typing import List, Tuple
 
 from sqlalchemy.orm import Session
 
-from app import crud
 from app.models.user import User
 from app.schemas.google_maps_api import GeocodeResponse, UserPreferences
 from app.schemas.place import Place
@@ -86,6 +87,75 @@ class Recommender:
         self.map_services = map_services
         self.candidate_fetcher = CandidateFetcher(db, user, map_services)
         self.user_preferences: UserPreferences = user_preferences
+        self.recommendation_weights = {
+            "interest": 5.0,
+            "search": 1.0,
+            "type": 2.0,
+            "rating": 1.0,
+            "recentness": lambda x: 1.5 if x <= 7 else 1.0,
+        }
+
+    def _generate_routes_matrix(
+        self, addresses: List[str], candidates: List[Place]
+    ) -> RoutesMatrix:
+        return RoutesMatrix(
+            self.map_services.get_distance_matrix_for_places(
+                self.db,
+                self.user,
+                origins=addresses,
+                destinations=[candidate.place_id for candidate in candidates],
+            )
+        )
+
+    # NOTE: 약간 어색하지만 중간에 candidate랑 get_distance_matrix_for_places애서 찾아온 주소가 일관성이 없어서 여기서 수정해줌
+    def _update_routes_matrix_addresses(
+        self, routes_matrix: RoutesMatrix, candidates: List[Place]
+    ):
+        routes_matrix.update_candidate_addresses(self.db, candidates)
+
+    def _filter_candidates_by_routes(
+        self, routes_matrix: RoutesMatrix, candidates: List[Place]
+    ) -> List[Place]:
+        return DistanceInfoFilter(routes_matrix, self.user_preferences).apply(
+            candidates
+        )
+
+    def _compute_scores_for_candidates(
+        self, candidates: List[Place]
+    ) -> List[Tuple[Place, float]]:
+        return [
+            (candidate, self.compute_recommendation_score(candidate))
+            for candidate in candidates
+        ]
+
+    def _compute_string_similarity(self, s1: str, s2: str) -> float:
+        return round(SequenceMatcher(None, s1, s2).ratio(), 2)
+
+    def _compute_recentness_weight(self, searched_date: datetime) -> float:
+        cur_utc_time = datetime.utcnow()
+        assert cur_utc_time > searched_date
+        days_since_searched = (cur_utc_time - searched_date).days
+        # NOTE: 최근 7일 이내에 검색된 장소는 더 높은 가중치를 받음
+        return self.recommendation_weights["recentness"](days_since_searched)
+
+    def compute_recommendation_score(self, place: Place):
+        score = 0
+
+        if place in self.user.interested_places:
+            score += self.recommendation_weights["interest"]
+
+        for history in self.user.search_history_relations:
+            similarity = self._compute_string_similarity(history.address, place.address)
+            recentness = self._compute_recentness_weight(history.created_at)
+            score += self.recommendation_weights["search"] * similarity * recentness
+
+        type_similarity = len(set(place.place_types) & set(self.user.preferred_types))
+
+        score += self.recommendation_weights["type"] * type_similarity
+
+        score += self.recommendation_weights["rating"] * place.rating
+
+        return score
 
     def recommend_places(
         self,
@@ -103,26 +173,22 @@ class Recommender:
             )
         return self.rank_candidates(candidates, addresses=addresses)
 
-    # TODO:추가적인 랭킹 또는 필터링 로직을 적용
     def rank_candidates(
         self,
         candidates: List[Place],
         addresses: List[str],
     ) -> List[Place]:
-        routes_matrix = RoutesMatrix(
-            self.map_services.get_distance_matrix_for_places(
-                self.db,
-                self.user,
-                origins=addresses,
-                destinations=[candidate.place_id for candidate in candidates],
-            )
+        routes_matrix = self._generate_routes_matrix(addresses, candidates)
+
+        self._update_routes_matrix_addresses(routes_matrix, candidates)
+
+        filtered_candidates = self._filter_candidates_by_routes(
+            routes_matrix, candidates
         )
+        scored_places = self._compute_scores_for_candidates(filtered_candidates)
 
-        # NOTE: 약간 어색하지만 중간에 candidate랑 get_distance_matrix_for_places애서 찾아온 주소가 일관성이 없어서 여기서 수정해줌
-        routes_matrix.update_candidate_addresses(self.db, candidates)
+        scored_places.sort(key=lambda x: x[1], reverse=True)
 
-        filtered_cadidates = DistanceInfoFilter(
-            routes_matrix, self.user_preferences
-        ).apply(candidates)
+        print(scored_places)
 
-        return filtered_cadidates
+        return [socre_place[0] for socre_place in scored_places]

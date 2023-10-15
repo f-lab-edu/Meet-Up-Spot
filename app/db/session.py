cb01f47a187ca8e4b973f3bdb64164cba6fbe98e
minwoo <minwoo9621@gmail.com>
2023-10-15 21:47:19 +0900

    Modify db connection pool

--- app/db/session.py
+++ app/db/session.py
@@ -4,5 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import get_app_settings
 
 settings = get_app_settings()
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL, pool_pre_ping=True, pool_size=15, max_overflow=25
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
